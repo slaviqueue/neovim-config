0ef1279e83498f05961b9c0ce50cfd09e473c6f8
slaviqueue <electricsemyon@gmail.com>
2023-08-16 22:49:45 +0300

    wip? wip

--- lua/slaviqueue/base.lua
+++ lua/slaviqueue/base.lua
@@ -1,8 +1,10 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
-vim.opt.shiftwidth = 2	
-vim.opt.tabstop = 2	
+vim.opt.numberwidth = 4
+vim.opt.signcolumn = 'yes'
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 vim.opt.hlsearch = false
 vim.opt.mouse = 'a'
 vim.opt.clipboard = 'unnamedplus'
@@ -14,3 +16,5 @@ vim.opt.smartcase = true
 vim.g.netrw_banner = false
 
 vim.opt.whichwrap:append 'h,l'
+
+vim.cmd 'highlight clear SignColumn'
