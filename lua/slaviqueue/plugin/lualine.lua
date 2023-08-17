1b1ed4c51b563c1dd5bbc053c2a38545d7bf4dbf
slaviqueue <electricsemyon@gmail.com>
2023-08-17 21:06:54 +0300

    updated lualine

--- lua/slaviqueue/plugin/lualine.lua
+++ lua/slaviqueue/plugin/lualine.lua
@@ -2,8 +2,6 @@ return {
   'nvim-lualine/lualine.nvim',
   opts = {
     options = {
-      icons_enabled = false,
-      theme = 'mountaineer',
       component_separators = '|',
       section_separators = '',
     },
