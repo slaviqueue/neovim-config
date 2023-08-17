22671e83f5a6066a4022ed5c26d5a0c5501e3a0e
slaviqueue <electricsemyon@gmail.com>
2023-08-17 21:05:46 +0300

    added comment toggler

--- lua/slaviqueue/plugin/misc.lua
+++ lua/slaviqueue/plugin/misc.lua
@@ -8,4 +8,18 @@ return {
       vim.keymap.set('n', '<leader>zm', ':ZenMode<cr>')
     end,
   },
+  {
+    'numToStr/Comment.nvim',
+    lazy = false,
+    config = function()
+      require('Comment').setup {
+        toggler = {
+          line = '<leader>/',
+        },
+        opleader = {
+          line = '<leader>/',
+        },
+      }
+    end,
+  },
 }
