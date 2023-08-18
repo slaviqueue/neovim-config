694526382518964c965cd75bc646ed9ec4065dc6
slaviqueue <electricsemyon@gmail.com>
2023-08-18 20:33:02 +0300

    reformatted nvim-tree config

--- lua/slaviqueue/plugin/nvim-tree.lua
+++ lua/slaviqueue/plugin/nvim-tree.lua
@@ -1,25 +1,25 @@
 return {
-	{ 'nvim-tree/nvim-web-devicons' },
-	{
-		'nvim-tree/nvim-tree.lua',
-		config = function()
-			require('nvim-tree').setup {
-				sort_by = 'case_sensitive',
-				view = {
-					width = 30,
-				},
-				renderer = {
-					group_empty = true,
-				},
-				update_focused_file = {
-					enable = true,
-					update_root = false,
-					ignore_list = {},
-				},
-			}
+  { 'nvim-tree/nvim-web-devicons' },
+  {
+    'nvim-tree/nvim-tree.lua',
+    config = function()
+      require('nvim-tree').setup {
+        sort_by = 'case_sensitive',
+        view = {
+          width = 30,
+        },
+        renderer = {
+          group_empty = true,
+        },
+        update_focused_file = {
+          enable = true,
+          update_root = false,
+          ignore_list = {},
+        },
+      }
 
-			local api = require 'nvim-tree.api'
-			vim.keymap.set('n', '<leader>e', api.tree.toggle)
-		end,
-	},
+      local api = require 'nvim-tree.api'
+      vim.keymap.set('n', '<leader>e', api.tree.toggle)
+    end,
+  },
 }
